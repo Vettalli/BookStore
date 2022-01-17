94b20d401f5ab2189701e60581ad8cffbff94f5c
vettalli <vettalli@mail.ru>
2022-01-17 12:00:30 +0400

    Created a Solution. Added Project BookStore and class Book

---
+++ domain/BookStore/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BookStore
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string Author { get; }
+        public string Title { get; }
+
+        public Book()
+        {}
+
+        public Book(int id, string author, string title)
+        {
+            Id = id;
+            Author = author;
+            Title = title;
+        }
+    }
+}
