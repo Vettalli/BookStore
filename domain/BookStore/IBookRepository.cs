f73c3db8dc28e071eeba9bb8f0498ded403ea8b2
vettalli <vettalli@mail.ru>
2022-01-17 12:08:55 +0400

    Added new project BookStore.Memory and implementation of IBookRepository

---
+++ domain/BookStore/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllByTitle(string title);
+    }
+}
---
+++ infrastructure/BookStore.Memory/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace BookStore.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        List<Book> books = new List<Book>
+        {
+            new Book(1, "Andrew W. Troelsen", "c# programming book")
+        };
+
+        public List<Book> GetAllByTitle(string title)
+        {
+            return books.Where(book => book.Title.Contains(title)).ToList();
+        }
+    }
+}
