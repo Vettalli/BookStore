e3475cd0827af29b3f26e0b4b373b20f9acb80b8
vettalli <vettalli@mail.ru>
2022-01-18 11:21:02 +0400

    Added new form for searching(Added controller for that)

--- infrastructure/BookStore.Memory/BookRepository.cs
+++ infrastructure/BookStore.Memory/BookRepository.cs
@@ -7,12 +7,20 @@ namespace BookStore.Memory
     {
         List<Book> books = new List<Book>
         {
-            new Book(1, "Andrew W. Troelsen", "c# programming book")
+            new Book(1, "Andrew W. Troelsen", "c# programming book"),
+            new Book(2, "Rybakov", "Arbats' children")
         };
 
         public List<Book> GetAllByTitle(string title)
         {
-            return books.Where(book => book.Title.Contains(title)).ToList();
+            List<Book> foundBooks = new List<Book>(books);
+
+            if (title != null)
+            {
+                foundBooks = books.Where(book => book.Title.Contains(title)).ToList(); 
+            }
+
+            return foundBooks;
         }
     }
 }
---
+++ presentation/BookStore.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private IBookRepository _bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = _bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
